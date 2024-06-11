8981f5f21cdaecb839572b1ddd9d12277cc64a0e
DimiStone <daltond@umich.edu>
2024-06-10 21:16:09 -0400

    Attempt 1

--- Lab7.py
+++ Lab7.py
@@ -9,10 +9,36 @@ Input(s):
 Processes:
 Output(s):
 """
+def convertMessage(message):
+    encryptedMessage = ""
+
+    for i in range(len(message)):
+        asciiNum = ord(message[i])
+        character = chr(asciiNum + 3)
+        encryptedMessage += character
+
+    return encryptedMessage
+
+def decodeMessage(decodeThis):
+    decodedMessage = ""
+
+    for i in range(len(decodeThis)):
+        asciiNum = ord(decodeThis[i])
+        character = chr(asciiNum - 3)
+        decodedMessage += character
+
+    return decodedMessage
 
 def main():
-    pass # replace this line with all of your solution code
+    message = input("Enter the message to be encrypted: ")
+    encryptedMessage = convertMessage(message)
 
+    print(f"Encrypted message = {encryptedMessage}\n")
+
+    decodeThis = input("Enter the cipher to be decrypted: ")
+    decodedMessage = decodeMessage(decodeThis)
+
+    print(f"Decrypted message = {decodedMessage}\n")
 
 if __name__ == '__main__':
     main()
